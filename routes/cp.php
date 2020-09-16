08197086a8425a01626bfdb813f49c910800629b
edalzell <erin@thedalzells.org>
2020-09-15 19:54:26 -0700

    nav menu stays open and highlighted (#9)

--- routes/cp.php
+++ routes/cp.php
@@ -7,8 +7,10 @@ use Silentz\Akismet\Http\Controllers\QueueController;
 use Silentz\Akismet\Http\Controllers\QueuesController;
 
 Route::name('akismet.')->prefix('akismet')->group(function () {
-    Route::get('/queues', [QueuesController::class, '__invoke'])->name('index');
-    Route::get('/queue/{form}', [QueueController::class, '__invoke'])->name('show');
+    Route::prefix('queues')->group(function () {
+        Route::get('/', [QueuesController::class, '__invoke'])->name('index');
+        Route::get('{form}', [QueueController::class, '__invoke'])->name('show');
+    });
     Route::name('spam.')->prefix('spam')->group(function () {
         Route::delete('/{form}/{id}', [DiscardSpamController::class, '__invoke'])->name('discard');
         Route::post('/{form}/{id}', [CreateSubmissionController::class, '__invoke'])->name('approve');
