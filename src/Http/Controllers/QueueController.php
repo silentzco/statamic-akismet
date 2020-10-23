39764ad5f9cebd99bad697893b9367087b02908b
edalzell <erin@thedalzells.org>
2020-10-23 16:57:43 -0700

    unneeded

--- src/Http/Controllers/QueueController.php
+++ src/Http/Controllers/QueueController.php
@@ -2,10 +2,7 @@
 
 namespace Silentz\Akismet\Http\Controllers;
 
-use Illuminate\Support\Facades\Storage;
-use Statamic\CP\Column;
 use Statamic\Forms\Form;
-use Statamic\Forms\Submission;
 use Statamic\Http\Controllers\Controller;
 
 class QueueController extends Controller
@@ -13,27 +10,5 @@ class QueueController extends Controller
     public function __invoke(Form $form)
     {
         return view('akismet::cp.queues.show', ['form' => $form]);
-
-        // $columns = [
-        //     Column::make('name')->label(__('akismet::cp.name')),
-        //     Column::make('email')->label(__('akismet::cp.email')),
-        //     Column::make('content')->label(__('akismet::cp.content')),
-        // ];
-
-        // $paths = Storage::files("spam/{$form->handle()}");
-
-        // $spam = collect($paths)->map(function ($path) use ($form) {
-        //     return tap(new Submission(), function ($submission) use ($path, $form) {
-        //         $submission->id(basename($path));
-        //         $submission->form($form);
-        //         $submission->data(unserialize(Storage::get($path)));
-        //     });
-        // });
-
-        // return view('akismet::cp.queues.show', [
-        //     'columns' => $columns,
-        //     'form' => $form,
-        //     'spam' => $spam,
-        // ]);
     }
 }
