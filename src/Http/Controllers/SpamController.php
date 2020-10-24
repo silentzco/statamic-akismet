bb75b3fb5f627032279cbae040501f3283f14614
edalzell <erin@thedalzells.org>
2020-10-23 17:43:33 -0700

    search & sort work

--- src/Http/Controllers/SpamController.php
+++ src/Http/Controllers/SpamController.php
@@ -4,15 +4,16 @@ namespace Silentz\Akismet\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 use Silentz\Akismet\Spam\Submission;
 use Statamic\Extensions\Pagination\LengthAwarePaginator;
 use Statamic\Facades\Config;
 use Statamic\Forms\Form;
 use Statamic\Forms\Submission as StatamicSubmission;
-use Statamic\Http\Controllers\Controller;
+use Statamic\Http\Controllers\CP\CpController;
 use Statamic\Http\Resources\CP\Submissions\Submissions;
 
-class SpamController extends Controller
+class SpamController extends CpController
 {
     public function show(Form $form, string $id)
     {
@@ -33,7 +34,7 @@ class SpamController extends Controller
         ]);
     }
 
-    public function index(Form $form, Request $request)
+    public function index(Form $form)
     {
         $paths = Storage::files("spam/{$form->handle()}");
 
@@ -45,10 +46,18 @@ class SpamController extends Controller
             });
         });
 
+        // Search submissions.
+        $submissions = $this->searchSubmissions($submissions);
+
+        // Sort submissions.
+        $sort = $this->request->sort ?? 'datestamp';
+        $order = $this->request->order ?? ($sort === 'datestamp' ? 'desc' : 'asc');
+        $submissions = $this->sortSubmissions($submissions, $sort, $order);
+
         // Paginate submissions.
         $totalSubmissionCount = $submissions->count();
         $perPage = request('perPage') ?? Config::get('statamic.cp.pagination_size');
-        $currentPage = (int) $request->page ?: 1;
+        $currentPage = (int) $this->request->page ?: 1;
         $offset = ($currentPage - 1) * $perPage;
         $submissions = $submissions->slice($offset, $perPage);
         $paginator = new LengthAwarePaginator($submissions, $totalSubmissionCount, $perPage, $currentPage);
@@ -56,5 +65,28 @@ class SpamController extends Controller
         return (new Submissions($paginator))
             ->blueprint($form->blueprint())
             ->columnPreferenceKey("forms.{$form->handle()}.columns");
+    }
+
+    private function searchSubmissions($submissions)
+    {
+        if (! $this->request->filled('search')) {
+            return $submissions;
+        }
+
+        return $submissions
+            ->filter(fn ($submission) => collect($submission->data())
+                ->filter(fn ($value) => $value && is_string($value))
+                ->filter(fn ($value) => Str::contains(strtolower($value), strtolower($this->request->search)))
+                ->isNotEmpty())
+            ->values();
+    }
+
+    private function sortSubmissions($submissions, $sortBy, $sortOrder)
+    {
+        return $submissions->sortBy(function ($submission) use ($sortBy) {
+            return $sortBy === 'datestamp'
+                ? $submission->date()->timestamp
+                : $submission->get($sortBy);
+        }, null, $sortOrder === 'desc')->values();
     }
 }
