7edb6b0f4485e32dea759df17a254cf3a65addc6
Erin Dalzell <erin@silentz.co>
2024-05-13 18:57:12 -0700

    Support Statamic 5 & Laravel 11 (#50)

--- src/Spam/AbstractSpam.php
+++ src/Spam/AbstractSpam.php
@@ -2,15 +2,15 @@
 
 namespace Silentz\Akismet\Spam;
 
-use nickurt\Akismet\Akismet as AkismetAPI;
+use nickurt\Akismet\Akismet;
 
 abstract class AbstractSpam implements Spam
 {
-    private AkismetAPI $akismetApi;
+    private Akismet $akismetApi;
 
     public function __construct()
     {
-        $this->akismetApi = app('Akismet');
+        $this->akismetApi = app(Akismet::class);
     }
 
     public function addToQueue(): void
