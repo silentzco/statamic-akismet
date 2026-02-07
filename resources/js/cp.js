4b8c44f5c6518aa87857cd18ec97468cfec031de
edalzell <erin@silentz.co>
2026-02-07 15:03:24 -0800

    Convert queue listing to Vue/Inertia

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -1,5 +1,7 @@
+import Queues from './pages/Queues.vue';
 import SpamListing from './components/SpamListing.vue';
 
 Statamic.booting(() => {
     Statamic.$components.register('spam-listing', SpamListing);
+    Statamic.$inertia.register('akismet::Queues', Queues);
 });
---
+++ resources/js/pages/Queues.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { Head } from '@statamic/cms/inertia';
+import { Badge, Heading, Listing } from '@statamic/cms/ui';
+
+const props = defineProps({
+    queues: { type: Array, required: true },
+})
+</script>
+
+<template>
+    <Head title="Spam Queues" />
+
+    <div>
+        <Heading :level=1 size="lg" class="mt-4">Queues</Heading>
+        <div class="mt-4">
+            <Listing
+                :allowCustomizingColumns="false"
+                :allowSearch="false"
+                :columns="[
+                    { field: 'title', label: 'Form' },
+                    { field: 'count', label: 'Spam' },
+                ]"
+                :items="queues"
+                :sortable="false"
+            >
+                <template #cell-title="{ row, value }">
+                    <a :href="row.link" v-html="value" />
+                </template>
+                <template #cell-count="{ row, value }">
+                    <Badge color="red" :text="value" pill />
+                </template>
+            </Listing>
+
+        </div>
+    </div>
+</template>
