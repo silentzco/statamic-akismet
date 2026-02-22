917fcd1fd65373b913a19cbbe811e13f0b35dc57
Erin Dalzell <erin@silentz.co>
2026-02-22 10:50:11 -0800

    Migrate config to settings (#66)
    * Updater
    
    * proper version

---
+++ src/UpdateScripts/MigrateConfigToSettings.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Silentz\Akismet\UpdateScripts;
+
+use Statamic\Facades\Addon;
+use Statamic\UpdateScripts\UpdateScript;
+
+class MigrateConfigToSettings extends UpdateScript
+{
+    public function shouldUpdate($newVersion, $oldVersion)
+    {
+        return $this->isUpdatingTo('6.1');
+    }
+
+    public function update()
+    {
+        $settings = collect(config('akismet.forms', []));
+
+        $settings->transform(function (array $config) {
+            $config['form'] = $config['handle'];
+            unset($config['handle']);
+
+            return $config;
+        });
+
+        Addon::get('silentz/akismet')
+            ->settings()
+            ->set('forms', $settings->all())
+            ->save();
+
+        $this->console()->info('Configuration migrated!');
+    }
+}
