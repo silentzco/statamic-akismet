e088f3f0c54c9c8eff7ca0bd79898ebd302d8132
edalzell <erin@silentz.co>
2026-02-21 16:21:30 -0800

    tidy

--- tests/Spam/SubmissionTest.php
+++ tests/Spam/SubmissionTest.php
@@ -2,7 +2,6 @@
 
 use Silentz\Akismet\Spam\Submission;
 use Statamic\Contracts\Addons\SettingsRepository;
-use Statamic\Facades\Addon;
 use Statamic\Facades\Form;
 use Statamic\Forms\Submission as StatamicSubmission;
 
@@ -23,18 +22,12 @@ it('handles form submissions', function (string $form, bool $expectation) {
 ]);
 
 it('can detect email only spam', function () {
-    $settings = new Settings(
-        Addon::get('silentz/akismet'),
-        ['forms' => ['contact_us' => ['email_field' => 'email']]]
+    $this->mock(SettingsRepository::class, fn ($mock) => $mock
+        ->shouldReceive('find')
+        ->with('silentz/akismet')
+        ->andReturn(settings(['forms' => ['contact_us' => ['email_field' => 'email']]]))
+        ->once()
     );
-
-    $this->mock(SettingsRepository::class, function ($mock) use ($settings) {
-        $mock
-            ->shouldReceive('find')
-            ->with('silentz/akismet')
-            ->andReturn($settings)
-            ->once();
-    });
 
     $submission = tap(new StatamicSubmission)
         ->form(Form::make('contact_us'))
